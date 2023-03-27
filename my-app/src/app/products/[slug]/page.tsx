f3ec5c83bd9adc3beca3ac50348d8096223cd0ec
bggbr <jaejung603@gmail.com>
2023-03-27 22:40:18 +0900

    layout, Link 실습

--- my-app/src/app/products/[slug]/page.tsx
+++ my-app/src/app/products/[slug]/page.tsx
@@ -1,10 +1,26 @@
+import { notFound } from "next/navigation";
+
 type Props = {
     params: {
         slug: string;
     };
 };
 
+// export const metadata = {
+//     title: "product slug",
+//     description: "product slug",
+// };
+
+export function generateMetadata({ params }: Props) {
+    return {
+        title: `제품의 이름: ${params.slug}`,
+    };
+}
+
 export default function Product({ params }: Props) {
+    if (params.slug === "nothing") {
+        notFound();
+    }
     return <h1>Product Pants ! {params.slug}</h1>;
 }
 
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
 export default function Product() {
-    return <h1>Product Index !</h1>;
+    return (
+        <>
+            <h1>제품 설명 첫 페이지 !</h1>
+            <ul>
+                <li>
+                    <Link href='/products/shirt'>shirt</Link>{" "}
+                </li>
+                <li>
+                    <Link href='/products/pants'>pants</Link>{" "}
+                </li>
+                <li>
+                    <Link href='/products/skirt'>skirt</Link>{" "}
+                </li>
+                <li>
+                    <Link href='/products/shoes'>shoes</Link>{" "}
+                </li>
+            </ul>
+        </>
+    );
 }
