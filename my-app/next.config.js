fdbd96e7f4c8f4ba063dc082ad9034a6839755d8
jaejeongbang <jaejeongbang@hyundai-autoever.com>
2023-04-04 19:09:52 +0900

    redirect

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -11,6 +11,21 @@ const nextConfig = {
             },
         ],
     },
+    async redirects() {
+        return [
+            // 예전에 사용된 페이지 같은 경우 처리하기 좋음 (SEO 측면)
+            {
+                source: "/products/deleted_forever",
+                destination: "/products",
+                permanent: true, // 영원히
+            },
+            {
+                source: "/products/deleted_temp",
+                destination: "/products",
+                permanent: false, // 일시적 이동
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
