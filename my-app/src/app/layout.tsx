0a88dab549e5d0c119a414295a7914a887162351
bggbr <jaejung603@gmail.com>
2023-04-08 16:21:41 +0900

    blog init

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -12,14 +12,14 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className={sans.className}>
+        <html lang='en' className={sans.className}>
             <body>
                 <header className={styles.header}>
                     <h1>Demo Note App</h1>
                     <nav>
-                        <Link href="/products">products</Link>
-                        <Link href="/contact">Contact</Link>
-                        <Link href="/about">About</Link>
+                        <Link href='/products'>products</Link>
+                        <Link href='/contact'>Contact</Link>
+                        <Link href='/about'>About</Link>
                     </nav>
                 </header>
                 {children}
