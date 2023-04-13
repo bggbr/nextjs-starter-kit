c4b290702066894108011ccd58b54e5a39688e91
bggbr <jaejung603@gmail.com>
2023-04-13 21:32:17 +0900

    Posts 날짜 기준 내림차순으로 정렬

--- my-app/src/components/PostsGrid.tsx
+++ my-app/src/components/PostsGrid.tsx
@@ -6,11 +6,13 @@ type Props = { posts: Post[] };
 export default function PostsGrid({ posts }: Props) {
     return (
         <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {posts.map((post) => (
-                <li key={post.path}>
-                    <PostCard post={post} />
-                </li>
-            ))}
+            {posts
+                .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+                .map((post) => (
+                    <li key={post.path}>
+                        <PostCard post={post} />
+                    </li>
+                ))}
         </ul>
     );
 }
