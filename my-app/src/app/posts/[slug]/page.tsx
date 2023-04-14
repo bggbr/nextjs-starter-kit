5fe9456e669c5266771f70fd6224fbf10e8953fd
bggbr <jaejung603@gmail.com>
2023-04-14 09:51:33 +0900

    이전, 다음 글 리팩토링

--- my-app/src/app/posts/[slug]/page.tsx
+++ my-app/src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import AbjacentPostCard from '@/components/AbjacentPostCard';
 import MarkdownViewer from '@/components/MarkdownViewer';
 import PostContent from '@/components/PostContent';
 import { getAllPosts, getPostContent } from '@/service/posts';
@@ -13,44 +14,16 @@ type Props = {
 
 export default async function page({ params: { slug } }: Props) {
     const post = await getPostContent(slug);
-    const { title, path } = post;
-    const allPosts = await getAllPosts();
-    const idx = allPosts.findIndex((post) => post.path === path);
-    const prev = idx - 1;
-    const next = idx + 1;
+    const { title, path, prev, next } = post;
 
     return (
         <article className="bg-gray-100 shadow-lg rounded-t-2xl overflow-hidden">
             <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
             <PostContent post={post} />
-            {/* <div className="flex">
-                {prev < 0 ? (
-                    ''
-                ) : (
-                    <Link href={`/posts/${allPosts[prev].path}`}>
-                        <div className="flex-1 relative h-[200px]">
-                            <Image className="w-full" src={`/images/posts/${allPosts[prev].path}.png`} alt={`${allPosts[prev].path}`} width={500} height={350} />
-                            <div className="absolute top-1/3 right-10 text-white">
-                                <h1 className="text-3xl font-bold text-right">{allPosts[prev].title}</h1>
-                                <p className="text-lg font-bold text-right">{allPosts[prev].description}</p>
-                            </div>
-                        </div>
-                    </Link>
-                )}
-                {next >= allPosts.length ? (
-                    ''
-                ) : (
-                    <Link href={`/posts/${allPosts[next].path}`}>
-                        <div className="flex-1 relative h-[200px]">
-                            <Image className="w-full" src={`/images/posts/${allPosts[next].path}.png`} alt={`${allPosts[prev].path}`} width={500} height={350} />
-                            <div className="absolute top-1/3 left-10 text-white">
-                                <h1 className="text-3xl font-bold text-left">{allPosts[next].title}</h1>
-                                <p className="text-lg font-bold text-left">{allPosts[next].description}</p>
-                            </div>
-                        </div>
-                    </Link>
-                )}
-            </div> */}
+            <section className="flex shadow-md">
+                {prev && <AbjacentPostCard post={prev} type="prev" />}
+                {next && <AbjacentPostCard post={next} type="next" />}
+            </section>
         </article>
     );
 }
--- my-app/src/service/posts.ts
+++ my-app/src/service/posts.ts
@@ -10,14 +10,22 @@ export type Post = {
     featured: boolean;
 };
 
-export type PostData = Post & { content: string };
+export type PostData = Post & { content: string; next: Post | null; prev: Post | null };
 
-export async function getPostContent(name: string): Promise<PostData> {
-    const filePath = path.join(process.cwd(), 'data', 'posts', `${name}.md`);
-    const metadata = await getAllPosts().then((posts) => posts.find((post) => post.path === name));
-    if (!metadata) throw new Error(`${name} is not found`);
+export async function getPostContent(fileName: string): Promise<PostData> {
+    const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
+    const posts = await getAllPosts();
+    const post = posts.find((post) => post.path === fileName);
+
+    if (!post) throw new Error(`${fileName} is not found`);
+
+    const index = posts.indexOf(post);
+
+    const next = index > 0 ? posts[index - 1] : null;
+    const prev = index < posts.length - 1 ? posts[index + 1] : null;
+
     const content = await readFile(filePath, 'utf-8');
-    return { ...metadata, content };
+    return { ...post, content, next, prev };
 }
 
 export async function getCategory() {
