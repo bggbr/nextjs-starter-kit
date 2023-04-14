781eeb3e40d7b0bba20da465ddd063d504b1ce6e
bggbr <jaejung603@gmail.com>
2023-04-14 09:11:20 +0900

    이전, 다음 글 포스트 이동 기능 구현

--- my-app/src/app/posts/[slug]/page.tsx
+++ my-app/src/app/posts/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import MarkdownViewer from '@/components/MarkdownViewer';
-import { getPostContent } from '@/service/posts';
+import PostContent from '@/components/PostContent';
+import { getAllPosts, getPostContent } from '@/service/posts';
 import Image from 'next/image';
+import Link from 'next/link';
 import { AiTwotoneCalendar } from 'react-icons/ai';
 
 type Props = {
@@ -10,21 +12,45 @@ type Props = {
 };
 
 export default async function page({ params: { slug } }: Props) {
-    const { title, description, date, path, content } = await getPostContent(slug);
+    const post = await getPostContent(slug);
+    const { title, path } = post;
+    const allPosts = await getAllPosts();
+    const idx = allPosts.findIndex((post) => post.path === path);
+    const prev = idx - 1;
+    const next = idx + 1;
 
     return (
         <article className="bg-gray-100 shadow-lg rounded-t-2xl overflow-hidden">
             <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
-            <section className="flex flex-col p-5">
-                <div className="flex self-end font-bold text-sky-600">
-                    <AiTwotoneCalendar className="h-[24px]" />
-                    <p className="font-semibold ml-2">{date.toString()}</p>
-                </div>
-                <h1 className="text-4xl font-bold mb-2">{title}</h1>
-                <p className="text-xl font-bold mb-2 ">{description}</p>
-                <div className="w-44 border-2 border-sky-600 mt-4 mb-8"></div>
-                <MarkdownViewer content={content} />
-            </section>
+            <PostContent post={post} />
+            {/* <div className="flex">
+                {prev < 0 ? (
+                    ''
+                ) : (
+                    <Link href={`/posts/${allPosts[prev].path}`}>
+                        <div className="flex-1 relative h-[200px]">
+                            <Image className="w-full" src={`/images/posts/${allPosts[prev].path}.png`} alt={`${allPosts[prev].path}`} width={500} height={350} />
+                            <div className="absolute top-1/3 right-10 text-white">
+                                <h1 className="text-3xl font-bold text-right">{allPosts[prev].title}</h1>
+                                <p className="text-lg font-bold text-right">{allPosts[prev].description}</p>
+                            </div>
+                        </div>
+                    </Link>
+                )}
+                {next >= allPosts.length ? (
+                    ''
+                ) : (
+                    <Link href={`/posts/${allPosts[next].path}`}>
+                        <div className="flex-1 relative h-[200px]">
+                            <Image className="w-full" src={`/images/posts/${allPosts[next].path}.png`} alt={`${allPosts[prev].path}`} width={500} height={350} />
+                            <div className="absolute top-1/3 left-10 text-white">
+                                <h1 className="text-3xl font-bold text-left">{allPosts[next].title}</h1>
+                                <p className="text-lg font-bold text-left">{allPosts[next].description}</p>
+                            </div>
+                        </div>
+                    </Link>
+                )}
+            </div> */}
         </article>
     );
 }
