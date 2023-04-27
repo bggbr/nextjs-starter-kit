05e264eea32a417ea3285966365a989e54441dd5
bggbr <jaejung603@gmail.com>
2023-04-27 22:03:31 +0900

    메일 전송

--- my-app/src/app/api/contact/route.ts
+++ my-app/src/app/api/contact/route.ts
@@ -1,4 +1,5 @@
-import * as yup from "yup";
+import * as yup from 'yup';
+import nodemailer from 'nodemailer';
 
 const bodySchema = yup.object().shape({
     from: yup.string().email().required(),
@@ -7,15 +8,34 @@ const bodySchema = yup.object().shape({
 });
 
 export async function GET(request: Request) {
-    return new Response("Hello, Next.js!");
+    return new Response('Hello, Next.js!');
 }
 
 export async function POST(req: Request) {
     const body = await req.json();
-    if (bodySchema.isValidSync(body)) return new Response("not format", { status: 400 });
+    if (bodySchema.isValidSync(body)) return new Response('not format', { status: 400 });
     const { from, subject, message } = body;
 
     // send email using nodemailer
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.AUTH_USER,
+            pass: process.env.AUTH_PASS,
+        },
+    });
 
-    return new Response("Hello, Next.js!");
+    let info = await transporter.sendMail({
+        from,
+        to: process.env.AUTH_USER,
+        subject,
+        text: message,
+        html: '<b>Hello world?</b>', // html body
+    });
+
+    console.log('info =', info);
+
+    return new Response('Hello, Next.js!');
 }
--- my-app/src/components/SendEmail.tsx
+++ my-app/src/components/SendEmail.tsx
@@ -1,11 +1,11 @@
-"use client";
-import React, { useState } from "react";
-import Banner from "./Banner";
+'use client';
+import React, { useState } from 'react';
+import Banner from './Banner';
 
 export default function SendEmail() {
-    const [email, setEmail] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
     const [isButton, setIsButton] = useState(false);
 
     const sendEmail = (e: React.FormEvent) => {
@@ -20,39 +20,24 @@ export default function SendEmail() {
             setIsButton(false);
         }, 2000);
 
-        fetch("/api/email", { method: "POST", body: JSON.stringify(formData), headers: { "Content-Type": "application/json" } });
+        console.log('formData = ', formData);
+
+        fetch('/api/contact', { method: 'POST', body: JSON.stringify(formData), headers: { 'Content-Type': 'application/json' } });
     };
 
-    const validateInputs = () => (email.trim() && subject.trim() && message.trim() ? true : (alert("Please fill in all fields."), false));
+    const validateInputs = () => (email.trim() && subject.trim() && message.trim() ? true : (alert('Please fill in all fields.'), false));
     return (
         <>
-            <h3 className='text-3xl font-bold text-center mb-10'>Or Send me an email</h3>
+            <h3 className="text-3xl font-bold text-center mb-10">Or Send me an email</h3>
             <Banner isButton={isButton} />
-            <form className='rounded-lg bg-slate-700 max-w-lg p-4 mx-auto' action='submit' onSubmit={sendEmail}>
-                <p className='text-white font-bold my-2'>Your Email</p>
-                <input
-                    name='from'
-                    className='pl-1 w-full'
-                    type='text'
-                    placeholder='Your Email'
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-                />
-                <p className='text-white font-bold my-2'>Subject</p>
-                <input
-                    name='subject'
-                    className='pl-1 w-full'
-                    type='text'
-                    placeholder='Subject'
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSubject(e.target.value)}
-                />
-                <p className='text-white font-bold my-2'>Message</p>
-                <textarea
-                    name='message'
-                    className='pl-1 w-full'
-                    rows={10}
-                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value)}
-                ></textarea>
-                <button className='w-full font-bold h-10 bg-yellow-300 text-slate-700 my-2'>Submit</button>
+            <form className="rounded-lg bg-slate-700 max-w-lg p-4 mx-auto" action="submit" onSubmit={sendEmail}>
+                <p className="text-white font-bold my-2">Your Email</p>
+                <input name="from" className="pl-1 w-full" type="text" placeholder="Your Email" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
+                <p className="text-white font-bold my-2">Subject</p>
+                <input name="subject" className="pl-1 w-full" type="text" placeholder="Subject" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSubject(e.target.value)} />
+                <p className="text-white font-bold my-2">Message</p>
+                <textarea name="message" className="pl-1 w-full" rows={10} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value)}></textarea>
+                <button className="w-full font-bold h-10 bg-yellow-300 text-slate-700 my-2">Submit</button>
             </form>
         </>
     );
