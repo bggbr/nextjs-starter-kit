0122320ef80975ec3c4ea7d57ab6a27bc2c4e425
bggbr <jaejung603@gmail.com>
2023-05-07 21:13:59 +0900

    react-thunk install

--- my-app/src/redux/store/index.ts
+++ my-app/src/redux/store/index.ts
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers.ts";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers.ts';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
